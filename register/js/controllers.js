34c53b0d7568e38e3d0578f57452e8b97502089f
Ha Lee <halee9@gmail.com>
2017-04-09 21:37:54 -0700

    kitchen ticket modify

--- register/js/controllers.js
+++ register/js/controllers.js
@@ -50,12 +50,12 @@ angular.module('myApp.controllers', [])
 		console.log('start print: ', queue.length, queue);
 		if (queue.length > 0) {
 			$scope.order_printing = queue.splice(0,1)[0];
-			// setTimeout(function(){
+			$timeout(function(){
 				console.log($scope.order_printing);
 				window.print();
-			// },0);
+			},0);
 		}
-		setTimeout(print_queue, 5000);
+		$timeout(print_queue, 5000);
 	})();
 
 
